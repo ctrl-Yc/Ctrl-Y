b405a70b59655ae0d7489ea551673b7beb92d4b1
Aoi-Kobayashi <oosabaaoi@gmail.com>
2025-05-29 15:33:14 +0900

    init

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+
+
+export const Home = () => {
+    return (
+        <div className="home">
+        <h1>Welcome to the Home Page</h1>
+        <p>This is a simple React application demonstrating routing and PWA capabilities.</p>
+        <p>Feel free to explore the app and check out the features!</p>
+        </div>
+    );
+}
