306f30517fbb1d7d4a739c0b88b041d9f9dffd61
hsmtDP-T <ittuyo.hashi244@gmail.com>
2025-06-16 02:32:54 +0900

    index.jsでAPI管理「動作確認出来てない」

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const mainRouter = require('./src/routes/index.js');
-const app = express();
+// const express = require('express');
+// const mainRouter = require('./src/routes/index.js');
+const app = require('./src/index.js');
 
 
-app.use('/api',mainRouter);
+// app.use('/api',mainRouter);
 
 
 const port = 3000
@@ -11,10 +11,10 @@ const port = 3000
 const url = process.env.SB_URL
 const anonKey = process.env.SB_SERVICE_ROLE
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
 
-})
+// })
 
 app.listen(port, () => {
   console.log(`サーバー起動 http://localhost:${port}`)
---
+++ backend/src/index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+
+//prisamaいんすたんす
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+
+app.use(express.json());
+
+//testできてない
+    //tasksの全件取得
+    app.get('/api/tasks/Allget',async (req,res)=>{
+        try{
+            const AllTasks =  await prisma.tasks.findMany();
+            res.status(200).json(AllTasks);
+        }catch (error){
+            console.log("tasksの全件取得エラー");
+            res.status(500).json({ message: 'tasksの全件取得エラー', error : error.message });
+        }
+    })
+
+    //taskのさくじょ
+    app.delete('/api/taske/taskDelete/:id',async (req,res)=>{
+        try{
+            const taskid = parseInt(req.params.id, 10);
+
+            const DeleteTask =  await prisma.tasks.delete({
+                where:{
+                    id: taskid
+                }
+            });
+            console.log("ID:"+taskid+"削除確認");
+        }catch (error){
+            console.log("taskの削除エラー");
+            res.status(500).json({ message: 'taskの削除エラー', error : error.message });
+        }
+    })
+
+
+//userの登録
+
+
+
+//一番下
+module.exports = app;
