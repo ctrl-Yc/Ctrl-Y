3076884408b1cdbafc2e12b25c2195b02281051d
AoiXD <oosabaaoi@gmail.com>
2025-06-20 14:53:19 +0900

    タスク取得APIを修正し、エラーハンドリングを改善。終了タスクの合計と給料合計金額を取得するAPIを追加。

--- backend/src/controllers/tasksControllers.js
+++ backend/src/controllers/tasksControllers.js
@@ -1,12 +1,11 @@
 //コントローラー
-
 const tasksServices = require("../services/tasksServices.js");
 
 exports.getAllTasks = async (req,res)=>{
     try{
         const AllTasks = await tasksServices.findAllTasks();
         res.status(200).json(AllTasks);
-    }catch{
-        res.status(500).json({ message : console.error.message});
+    }catch (error) {
+        res.status(500).json({ message : error.message});
     }
 }
