6d0b94add8f646372198276ddc20a13c4d9e9095
hsmt-T <ittuyo.hashi244@gmail.com>
2025-07-03 01:17:24 +0900

    parentOnly修正

--- backend/src/middlewares/parentOnly.js
+++ backend/src/middlewares/parentOnly.js
@@ -1,10 +1,12 @@
 function parentOnly(req,res,next) {
     try{
         if(!req.user || req.user.role !== 'parent') {
-            return res.this.status(403).json({ message:'この操作は親のアカウントでしか実行できません！' })
+            return res.status(403).json({ message:'この操作は親のアカウントでしか実行できません！' })
         }
         next();
     }catch (error) {
         next(error);
     }
 }
+
+module.exports = parentOnly;
