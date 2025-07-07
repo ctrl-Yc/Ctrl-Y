f8ad36518aee809e4758a2e1d7b2c53910d8df53
leon <2240206ecc@gmail.com>
2025-07-07 16:10:45 +0900

    InputFieldコンポーネントにdisabledプロパティを追加し、AccountSettingsコンポーネントでメールアドレスとパスワード変更機能を実装

--- frontend/src/components/common/InputField.jsx
+++ frontend/src/components/common/InputField.jsx
@@ -1,10 +1,11 @@
-export const InputField = ({ type, placeholder = '', value, onChange, className = '' }) => {
+export const InputField = ({ type, placeholder = '', value, onChange, className = '', disabled }) => {
   return (
     <input 
       type={type} 
       placeholder={placeholder} 
       value={value} onChange={onChange} 
       className={className}
+      disabled={disabled}
     />
   );
 };
