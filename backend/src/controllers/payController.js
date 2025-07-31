9743503d383ebf647cc6be27ed71779a7caf92b1
hsmt-T <ittuyo.hashi244@gmail.com>
2025-08-01 01:22:04 +0900

    ここにわかりやすい変更内容を書く

--- backend/src/controllers/payController.js
+++ backend/src/controllers/payController.js
@@ -4,7 +4,6 @@ exports.payroll = async (req,res) => {
     try{
         const parent_id = req.user.user_id;
         const payroll = await payServices.payroll(parent_id);
-        
         res.status(200).json(payroll);
     } catch (error) {
         console.log("給与リスト取得エラー", error.message);
