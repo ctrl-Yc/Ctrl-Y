007a7d66fd6889fd55de6cca006ab3b807f89a95
AoiXD <oosabaaoi@gmail.com>
2025-09-03 19:59:52 +0900

    fix: CORSオプションのフロントエンドURLを正規表現に変更

--- backend/src/lib/cors.js
+++ backend/src/lib/cors.js
@@ -2,7 +2,7 @@
 const cors = require("cors");
 
 const corsOptions = {
-    origin: ["http://localhost:5173", "http://localhost:5174", "https://frontend*"],
+    origin: ["http://localhost:5173", "http://localhost:5174", /^https:\/\/frontend(?:-[a-z0-9-]+)?\.vercel\.app$/],
     credentials: true,
 };
 
